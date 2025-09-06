326091072214614bd95a7124293dd9f13e6b22b6
YH <csw03105@naver.com>
2025-09-06 23:54:16 +0900

    20250906_codingTest

---
+++ src/programmers/Java_Level_1.java
@@ -0,0 +1,51 @@
+package programmers;
+
+
+// 실행 Ctrl + F11
+public class Java_Level_1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		String s = "1 5 4 8 21";
+//		String answer = "";
+//		
+//		String sArray[] = null;
+//		
+//		sArray = s.split(" ");
+//		
+//		for(int i = 0; i < sArray.length; i++){
+//			int numArr[] = new int[sArray.length];
+//			
+//			numArr[i] = Integer.parseInt(sArray[i]);
+//
+////		    System.out.print(sArray[i] + " ");
+////		    System.out.print(numArr[i] + " ");
+//		    
+//		}
+		
+		//======================================================================================== Lv.0 
+		//평균 구하기
+		p_12944(new int[]{1,1,2,2,2});
+		
+		
+		
+
+	}
+	
+	//----------------------------------------------------------------------------- 2025-09-6(토)
+	//평균 구하기
+	public static void p_12944(int[] arr) {
+        double answer = 0;
+        int sum = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+            sum += arr[i];
+        }
+        
+        answer = (double)sum / arr.length;
+           
+        System.out.println("[p_12944] 평균 구하기 : " + answer);
+	}
+
+
+}
