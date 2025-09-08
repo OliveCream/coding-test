558aa48c095ef4f68bb7abc7edd813619d62aef7
YH <csw03105@naver.com>
2025-09-08 22:47:50 +0900

    20250908_codingTest

--- src/programmers/Java_Level_1.java
+++ src/programmers/Java_Level_1.java
@@ -1,24 +1,41 @@
 package programmers;
 
-
 // 실행 Ctrl + F11
 public class Java_Level_1 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+//		String s = "1 5 4 8 21";
+//		String answer = "";
+//		
+//		String sArray[] = null;
+//		
+//		sArray = s.split(" ");
+//		
+//		for(int i = 0; i < sArray.length; i++){
+//			int numArr[] = new int[sArray.length];
+//			
+//			numArr[i] = Integer.parseInt(sArray[i]);
+//
+////		    System.out.print(sArray[i] + " ");
+////		    System.out.print(numArr[i] + " ");
+//		    
+//		}
 		
 		//*** Lv.0 ***
 		//평균 구하기
 		p_12944(new int[]{1,1,2,2,2});
+		
+		//평균 구하기
+		p_12947(15246);
 		
 		
 		
 
 	}
 	
-	//----------------------------------------------------------------------------- 2025-09-6(토)
+	//----------------------------------------------------------------------------- 2025-09-06(토)
 	//평균 구하기
 	public static void p_12944(int[] arr) {
         double answer = 0;
@@ -30,6 +47,36 @@ public class Java_Level_1 {
         answer = (double)sum / arr.length;
            
         System.out.println("[p_12944] 평균 구하기 : " + answer);
+	}
+	
+	//-----------------------------------------------------------------------------
+	//하샤드 수
+	public static void p_12947(int x) {
+        boolean answer = true;
+        int sum=0;
+        String number = Integer.toString(x);
+        for(int i=0 ; i<number.length() ; i++){
+            sum += Character.getNumericValue(number.charAt(i));
+            //sum += number.charAt(i) - '0';		//아스키코드값을 정수로 변환
+        }
+        if(x%sum != 0){
+            answer = false;
+        }
+        
+        //다른 방법 - 매개변수 int를 %로 일의자리 수를 뽑아 더하는 방법
+        /*
+		int number = x;
+		int sum = 0;
+		
+		while(number > 0){
+		    sum += number % 10;
+		    number /= 10;
+		}
+		if(x%sum != 0){
+		    answer = false;
+		}
+		*/
+        System.out.println("[p_12947] 하샤드 수 : " + answer);
 	}
 
 
