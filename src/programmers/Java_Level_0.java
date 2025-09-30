ec0aca04516e11b63a0a8204b873e168037cf020
YH <csw03105@naver.com>
2025-09-30 23:59:01 +0900

    20250930_codingTest

--- src/programmers/Java_Level_0.java
+++ src/programmers/Java_Level_0.java
@@ -303,6 +303,11 @@ public class Java_Level_0 {
 	}
 	
 	//-----------------------------------------------------------------------------
-
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = Arrays.stream(numlist)
+                             .filter(num -> num%n == 0)
+                             .toArray();
+        return answer;
+    }
 
 }
